ccc7948407aad730858cec6ff21f8c0dd36485f2
abhishek <abhishekmazumder69@gmail.com>
2022-07-22 12:12:49 +0530

    Header/Navbar Implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
+import "./style.scss"
+import "./media-query.css"
 import About from './pages/About';
 import AddEditBlog from './pages/AddEditBlog';
 import Detail from './pages/Detail';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Header from './components/Header';
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
       <ToastContainer />
 			<Routes>
 				<Route path="/" element={<Home />} exact />
